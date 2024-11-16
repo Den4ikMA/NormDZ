20f86ad7dae826d8fdf2e3eeb6433f3cbdfccdf2
Daniil <Shypgdugd577@gmail.com>
2024-11-16 12:40:42 +0300

    Update сжатие3.py

--- сжатие3.py
+++ сжатие3.py
@@ -133,6 +133,6 @@ def process_images_in_directory(input_directory):
             decompress_text_to_image(compressed_text, output_image_path)
             print(f'Saved reconstructed image to {output_image_path}')
 
-# Example usage
+# Example usage input_direcrory=r'путь к вашей папке с картинкой'
 input_directory = r'C:\pics'
 process_images_in_directory(input_directory)
